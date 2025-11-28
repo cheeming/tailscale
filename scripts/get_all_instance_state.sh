869bd6ba018b00a0a0bb3d34d14949696b516ccc
Chew Chee Ming <cheeming@gmail.com>
2025-11-28 16:58:19 +0800

    Add us-west-1

--- scripts/get_all_instance_state.sh
+++ scripts/get_all_instance_state.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for REGION in us-east-1 ap-northeast-1 ap-northeast-2 ap-east-1 ap-southeast-1 ap-southeast-5; do
+for REGION in us-east-1 us-west-1 ap-northeast-1 ap-northeast-2 ap-east-1 ap-southeast-1 ap-southeast-5; do
     echo "===================="
     echo "${REGION}"
     echo "===================="
